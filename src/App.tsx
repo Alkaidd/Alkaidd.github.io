b5dcf36ad184501852f402b8e3266205e3123fdb
罗凯腾 <315776941@qq.com>
2024-03-28 17:28:56 +0800

    feat: do polyfill for structuredCloned

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import './App.css'
 import TetrisScreen from './components/Tetris'
 
+function polyfill() {
+  // @ts-expect-error do polyfill
+  if (window.structuredClone) {
+    return
+  }
+  window.structuredClone = (val: any) => {
+    return JSON.parse(JSON.stringify(val))
+  }
+}
+polyfill()
+
 function App() {
   return (
     <div w-full h-full flex items-center justify-center>
