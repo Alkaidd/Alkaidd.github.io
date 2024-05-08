84cf19e3706ae1c957c756fca75dcd8cdd6c6f24
Alkaidd <315776941@qq.com>
2024-05-09 00:05:10 +0800

    -m release: update article

---
+++ assets/batchSamplersUniformGroup-DbfM15D2.js
@@ -0,0 +1,1 @@
+import{M as e}from"./index-BfOhw5-r.js";import{U as o}from"./colorToUniform-CBk5oLl_.js";const t=new Int32Array(e);for(let r=0;r<e;r++)t[r]=r;const i=new o({uTextures:{value:t,type:"i32",size:e}},{isStatic:!0});export{i as b};
